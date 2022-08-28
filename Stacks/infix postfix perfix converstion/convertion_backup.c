2d574cd2ac6456e654d2f6029fd2f020b94bc67e
Aabhas Kumar Jha <abhasjha03@gmail.com>
2022-08-28 15:29:21 +0530

    added infix to prefix convertion in C

---
+++ Stacks/infix postfix perfix converstion/convertion_backup.c
@@ -0,0 +1,163 @@
+#include <stdio.h>
+#include <stdlib.h> // to use the maloc() and free() function
+#include <stdbool.h> // to use the bool datatype (true and false)
+#include <string.h> // for string operations (here strlen)
+#include <ctype.h> // for isalpha() and isdigit() functions
+
+// ===== OPERATIONAL VARIABLES =====
+
+char* infix_expression;
+char* output; // to store the postfix or prefix expression
+
+char* stack; // stack used for convertion operations
+int capacity;
+int top = -1;
+
+char operators[5] = "+-*/^";
+
+// ===== STACK OPERATIONS =====
+
+void push(int charector){
+    top++;
+    stack[top] = charector;
+}
+
+char pop(){
+    char charector = stack[top];
+    top--;
+    return charector;
+}
+
+// ===== PRESEDENCE CHECKING AND OPERATOR OPERATIONS =====
+
+int get_precedence(char operator){
+    switch (operator){
+        case '+':
+        case '-':
+            return 1;
+            break;
+        case '*':
+        case '/':
+            return 2;
+            break;
+        case '^':
+            return 3;
+            break;
+        default:
+            return 0;
+            break;
+    }
+}
+
+// fxn to find if a charector is an operand or not
+bool isOperand(char charector){
+    return (isdigit(charector) || isalpha(charector)) ? true : false;
+}
+
+bool isOperator(char charector){
+    char* found = strchr(operators, charector);
+    return (found == NULL) ? false : true;
+}
+
+// ===== CONVERSTION FUNCTIONS =====
+
+bool check_bracket_order(char* infix, int capacity){
+    int brackets_balance = 0;
+    for (int position = 0; position < capacity; position++){
+        char bracket = infix[position];
+        if (bracket == '('){
+            brackets_balance++; // symolises the number of openning brackets
+        } else if (bracket == ')'){
+            brackets_balance--; // neutalises an open bracket whenever a closed bracket is encountered
+        }
+    }
+    return (brackets_balance == 0) ? true : false;
+}
+
+char* infix_to_postfix(){
+
+    int index = 0;
+    char stack_top_op;
+
+    for (int position = 0; position < capacity; position++){
+        // getting what charector to analyse in the infix expression
+        char charector = infix_expression[position];
+        // if charector is an operator
+        if (isOperator(charector)){
+            char infix_op = charector;
+            int infix_op_precedence = get_precedence(infix_op);
+            stack_top_op = pop();
+            if (stack_top_op == '\0'){
+                push(infix_op);
+            }
+            while (stack_top_op != '\0'){
+                if (stack_top_op == '('){
+                    push(infix_op);
+                }else{
+                    push(stack_top_op);
+                    int stack_top_op_precedence = get_precedence(stack_top_op);
+                    if (stack_top_op_precedence < infix_op_precedence){
+                        push(infix_op);
+                    } else{
+                        stack_top_op = pop();
+                        output[index] = stack_top_op;
+                        index++;
+
+                    }
+                }
+                stack_top_op = pop();
+            }
+        } 
+        // if charector is an operand we append it to the output expresssion
+        else if(isOperand(charector)){
+            output[index] = charector;
+            index++;
+        } 
+        // if charector is an open paranthesis we push it to the stack
+        else if (charector == '('){
+            push(charector);
+        } 
+        // if charctor is a closing paranthesis we pop all operators and append it the the output expression till an open paranthesis is encountered
+        else if (charector == ')'){
+            char popped = pop();
+            while (popped != '('){
+                output[index] = popped;
+                index++;
+                popped = pop();
+            }
+        }
+    }
+    while (stack_top_op != '\0'){
+        output[index] = stack_top_op;
+        index++;
+    }
+    return output;
+}
+
+// ===== DRIVER CODE =====
+
+int main(){
+    
+    int min_capacity = 3; // min capacity of infix expression must be 3 because there must be atleast one operator bet
+    infix_expression = malloc(min_capacity * sizeof(char));
+    scanf("%s", infix_expression);
+
+    capacity  = strlen(infix_expression);
+    stack = (char*) malloc(capacity * sizeof(char));
+    output = (char*) malloc(capacity * sizeof(char));
+
+    bool bracets_in_order = check_bracket_order(infix_expression, capacity);
+
+    if (bracets_in_order){
+        char* postfix = infix_to_postfix();
+        printf("POSTFIX : %s", postfix);
+
+    } else{
+        printf("INVALID INFIX EXPRESSION");
+    }
+
+    free(infix_expression);
+    free(stack);
+    
+    return 0;
+}
---
+++ Stacks/infix postfix perfix converstion/infix2postfix.c
@@ -0,0 +1,196 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+// this program converts infix to postfix
+// operators {"(",")","+","-","*","/","^"}
+
+#define MAX 200
+int stack[MAX];
+int top = -1;
+// push function
+void push(char item)
+{
+    if (top >= MAX - 1)
+    {
+        printf("stack is overflowing\n");
+    }
+    else
+    {
+        top++;             // -1+1=0 so now it allows us to add an item at the 0th position and will keep track of the yop most element
+        stack[top] = item; // places an object on the stack top
+        // doesn't return anything as the function type is void
+    }
+}
+// pop functiom
+char pop()
+{
+    char item;
+    if (top < 0)
+    {
+        printf("stack is underflowing\n");
+        exit(1);
+    }
+    else
+    {
+        item = stack[top];
+        top = top - 1;
+
+        return (item); // gets u the item that was poped could be an operator or bracked and all that is strored in x
+        // note that this function has the type char so we have to store item in a char type variable
+    }
+}
+int Operator(char symbol)
+{
+    if (symbol == '^' || symbol == '*' || symbol == '/' || symbol == '+' || symbol == '-')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int OperatorPrecedence(char symbol)
+{
+    if (symbol == '^')
+    {
+        return 3; // returns 3 as a value to symbol
+    }
+    else if (symbol == '/' || symbol == '*')
+    {
+        return 2; // returns 2 as a value to symbol
+    }
+    else if (symbol == '+' || symbol == '-')
+    {
+        return 1; // returns 1 as a value to symbol
+    }
+    else
+    {
+        return 0;
+    }
+}
+void Brackets(char infix[])
+{
+    int i = 0, brac_count = 0;
+    while (infix[i] != '\0')
+    {
+        if (infix[i] == '(')
+        {
+            brac_count++;  //as this index increase it must be neutralized by the negative counter 
+        }  
+
+        else if (infix[i] == ')')
+        {
+            brac_count--;   // this neutralizes the positive counter
+            if (brac_count < 0)  
+            /* if the counter has negavtive value then there must be more right brackets 
+            than the left ones so the brackets are unbalanced*/
+            {
+                break;
+            }
+        }
+        i++;  //helps in traversing through the string
+    }
+
+    if (brac_count == 0)
+    {
+        printf("\n=================BRACKETS are Balanced================\n");
+    }
+    else
+    {
+        printf("\n********************BRACKETS are NOT Balanced****************\n");
+        printf("\nPlease check the expression you have entered\n");
+        exit(0);
+    }
+}
+void Infix2Postfix(char infix[], char postfix[])
+{
+    int i = 0, j = 0;
+    char item = infix[i];
+    char x;
+
+    push('(');          // first step is pushing a left bracket into the stack
+    strcat(infix, ")"); // appending a right bracket to the expression so that we can pop all elements at its encounter
+
+    while (item != '\0')
+    {
+        if (item == '(')
+        {
+            push(item);
+        }
+        else if (isdigit(item) || isalpha(item))
+        {
+            postfix[j] = item; // variables or digits go directly to the expression
+            j++;
+        }
+        else if (Operator(item) == 1)
+        {
+            x = pop(); /* NOW x has the stack top element.
+            'item' hasn't been pushed to stack so we haven't poped the incoming operator
+             if the while condition fails then we just push back x to the stack top  */
+            while (Operator(x) == 1 && OperatorPrecedence(x) >= OperatorPrecedence(item))
+            {
+                postfix[j] = x;
+                j++;
+                x = pop();
+            }
+            push(x);
+            push(item);
+        }
+        else if (item == ')')
+        {
+            x = pop();
+            while (x != '(')
+            {
+                postfix[j] = x;
+                j++;
+                x = pop();
+            }
+        }
+        else
+        {
+            /*incoming symbol is not an expression nor an operator*/
+            printf("\ninvalid expression!!\n");
+            exit(1);
+        }
+        i++;
+        item = infix[i];
+        /*so first here item is updated and we take a character from infix
+        expression and we increase i so that we can traverse the expression*/
+    }
+    postfix[j] = '\0';
+}
+
+int main(int argc, char const *argv[])
+{
+    char infix_input[MAX], postfix_output[MAX];
+    printf("\nEnter an INFIX EXPRESSION:\n");
+    gets(infix_input);
+    /*inputs a string and then are storing it in a array that we use later on*/
+
+    Brackets(infix_input); /*CHECKING FOR BALANCED PARANTHESIS*/
+
+    Infix2Postfix(infix_input, postfix_output);
+    /*we call the function for the conversion. We give it 2 arguments one is an input
+    string and the other is an empty string for the function can store its output*/
+
+    printf("\nPostfix Expression is :");
+    puts(postfix_output);
+
+    printf("\n");
+    return 0;
+}
+
+
+// CORRECTIONS :
+
+// LINE 169 - use scanf("%s") instead of gets : gives the same output
+
+// USE dynamic memory allocation (malloc and free) to make the program space complexity efficient, then use a for loop to traverse through the input infix expression
+
+// use appropriate variables example : infix[i], for this use charector && for the function name brackets, use check_brackets
+
+// use ternary operators in place of small if else statemets like in bracket checking
+
+// use bool instead of void for bracker checker and check if true then do the converstion otherwise dont, do not use exit() in the brackets functions to stop program execution
